6fc86a856001251ca7e40aedffad68f170516946
jungwoogyun <jwg8910@naver.com>
2022-09-30 17:50:33 +0900

    02 기본 out 메서드

---
+++ src/Ch01/C02BasicOutputMethod.java
@@ -0,0 +1,35 @@
+package Ch01;
+
+public class C02BasicOutputMethod {
+
+	public static void main(String[] args) {
+		
+		//00 System.out : 표준 출력 스트림 
+		//01 System.out.print()
+		
+		//ESCAPE 문자 사용
+		// \n : 개행문자
+		// \b : 백스페이스(C언어) 
+		// \t : 탭길이(기본8칸) 만큼 이동 
+		System.out.print("HELLO WORLD\n");
+		System.out.print("HE\tLLO WORLD\n");
+		System.out.print("HELLO WORLD\n");
+		
+		// 문제
+		//    * 
+		//   ***
+		//  *****
+		// *******
+		//*********
+		
+		
+		//02 System.out.printf()
+		//03 System.out.println()
+		
+		
+		
+		
+		
+	}
+
+}
