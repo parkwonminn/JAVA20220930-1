666a44b4fe0f0da661f7fc740f730258a357149d
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-10-05 09:50:03 +0900

    testupload

---
+++ src/C00EXEC/Ch01Practicetest.java
@@ -0,0 +1,12 @@
+package C00EXEC;
+
+public class Ch01Practicetest {
+
+	public static void main(String[] args) {
+		System.out.printf("%s\t%s\t%s\t%s\n", "NO", "이름", "나이", "주소");
+		System.out.printf("%d\t%s\t%d\t%s\n", 1, "김광수", 35, "대구");
+		System.out.printf("%d\t%s\t%d\t%s\n", 2, "이태수", 55, "울산");
+		System.out.printf("%d\t%s\t%d\t%s\n", 3, "이지성", 44, "김해");
+	}
+
+}
